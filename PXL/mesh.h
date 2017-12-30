6ed74d6b59484866982dc4313c9174faa62496fe
Mel Florance <mel.florance@gmail.com>
2017-12-30 16:46:14 +0100

    Externalize vertex class to its own file.

--- PXL/mesh.h
+++ PXL/mesh.h
@@ -5,26 +5,7 @@
 #include <string>
 #include "obj_loader.h"
 #include "transform.h"
-
-class Vertex
-{
-public:
-	Vertex(const glm::vec3& pos, const glm::vec2& texCoord, const glm::vec3& normal = glm::vec3(0.0f, 0.0f, 0.0f)) {
-		this->pos = pos;
-		this->texCoord = texCoord;
-		this->normal = normal;
-	}
-
-	inline glm::vec3* getPos() { return &pos; }
-	inline glm::vec2* getTexCoord() { return &texCoord; }
-	inline glm::vec3* getNormal() { return &normal; }
-
-private:
-	glm::vec3 pos;
-	glm::vec2 texCoord;
-	glm::vec3 normal;
-};
-
+#include "Vertex.h"
 
 class Mesh
 {
---
+++ PXL/vertex.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+class Vertex
+{
+public:
+	Vertex(const glm::vec3& pos, const glm::vec2& texCoord, const glm::vec3& normal = glm::vec3(0.0f, 0.0f, 0.0f)) {
+		this->pos = pos;
+		this->texCoord = texCoord;
+		this->normal = normal;
+	}
+
+	inline glm::vec3* getPos() { return &pos; }
+	inline glm::vec2* getTexCoord() { return &texCoord; }
+	inline glm::vec3* getNormal() { return &normal; }
+
+private:
+	glm::vec3 pos;
+	glm::vec2 texCoord;
+	glm::vec3 normal;
+};
